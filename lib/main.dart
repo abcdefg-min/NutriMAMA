f004c2fb6b967dc657034eae1ee898155c0ce9a7
abcdefg_minSUng <vikavolk531@mail.ru>
2025-10-11 15:55:38 +0300

    главная страница

--- lib/main.dart
+++ lib/main.dart
@@ -3,47 +3,80 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: NutriMAMAScreen(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NutriMAMAScreen extends StatefulWidget {
+  const NutriMAMAScreen({super.key});
+
+  @override
+  State<NutriMAMAScreen> createState() => _NutriMAMAScreenState();
+}
+
+class _NutriMAMAScreenState extends State<NutriMAMAScreen> {
+  final TextEditingController _weghtController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _weghtController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+  void _calculate() {
+    final weight = _weghtController.text;
+    final age = _ageController.text;
+    print('Вес: $weight кг, Возраст: $age недель');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('HTTP POST Example')),
-        body: const Center(child: PostButton()),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("NutriMAMA")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _weghtController,
+              decoration: const InputDecoration(
+                labelText: "Вес ребёнка в кг",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _ageController,
+              decoration: const InputDecoration(
+                labelText: "Возраст",
+                hintText: "Введите возраст",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 4),
+            const Padding(
+              padding: EdgeInsets.only(left: 12.0),
+              child: Text(
+                "в неделях",
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Center(
+              child: ElevatedButton(
+                onPressed: _calculate,
+                child: const Text("Рассчитать"),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
 
-class PostButton extends StatelessWidget {
-  const PostButton({super.key});
-
-  Future<void> _sendPostRequest() async {
-    final url = Uri.https('httpbin.org','/post');
-    final data = {'name': 'Flutter USer', 'message': 'Hello from Flutter'};
-
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(data),
-    );
-
-    if (response.statusCode == 200) {
-      print('Успех: ${response.body}');
-    }
-    else{
-      print('Ошибка: ${response.statusCode}');
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: _sendPostRequest, 
-    child: const Text('Отрпавить POST-запрос'),
-    );
-  }
-}
