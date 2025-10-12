88b33ee6990126267694e90d0f0f2f7d402d6079
abcdefg_minSUng <vikavolk531@mail.ru>
2025-10-12 20:33:36 +0300

    animtion загрузки

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,9 @@
 import "dart:async";
+import "package:loading_animation_widget/loading_animation_widget.dart";
+
 import 'nutriMAMAScreen.dart';
 import "package:flutter/material.dart";
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 5), () {
       Navigator.push(
         context, 
         MaterialPageRoute(builder: (context) => const NutriMAMAScreen()));
@@ -25,8 +28,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 240, 235, 241),
       body: Center(
-        child: Image.asset("assets/images/nutriMAMA.png")
-        
+        //child: Image.asset("assets/images/nutriMAMA.png")
+        child: LoadingAnimationWidget.threeRotatingDots(
+          color: const Color.fromARGB(255, 207, 161, 200),
+          size: 100
+          ),
       ),
     );
   }
