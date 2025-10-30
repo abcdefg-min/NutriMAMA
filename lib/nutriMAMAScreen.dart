ac39e72ebeba0f351d87308251c7347d94ece790
abcdefg_minSUng <vikavolk531@mail.ru>
2025-10-30 10:47:53 +0300

    форма бронирования с картинкой nutri

--- lib/nutriMAMAScreen.dart
+++ lib/nutriMAMAScreen.dart
@@ -15,7 +15,10 @@ class _NutriMAMAScreenState extends State<NutriMAMAScreen> {
   final TextEditingController _weghtController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
 
-  final List<String> _dropDown = ['Грудное вскармливание','Искуственное вскармливание'];
+  final List<String> _dropDown = [
+    'Грудное вскармливание',
+    'Искуственное вскармливание',
+  ];
   String _drop = 'Грудное вскармливание';
 
   // bool _showTable = false;
@@ -26,18 +29,18 @@ class _NutriMAMAScreenState extends State<NutriMAMAScreen> {
     _ageController.dispose();
     super.dispose();
   }
-  
+
   void _calculate() {
     //print('Функция вызвана');
     final form = _formKey.currentState;
-    
+
     if (form == null || !form.validate()) {
       //print('Форам не валидна');
       return;
     }
 
     double? weight = double.tryParse(_weghtController.text);
-    int? age = int.tryParse(_ageController.text); 
+    int? age = int.tryParse(_ageController.text);
     // print('Вес: $weight кг, Возраст: $age недель');
     //делаем POST запрос
     http
@@ -58,142 +61,140 @@ class _NutriMAMAScreenState extends State<NutriMAMAScreen> {
     // });
     if (weight != null && age != null) {
       Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => TableScreen(weight: weight, age: age),
-      ),
-    );
-    }
-    else{
+        context,
+        MaterialPageRoute(
+          builder: (context) => TableScreen(weight: weight, age: age),
+        ),
+      );
+    } else {
       return;
     }
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 246, 238, 230),
       //appBar: AppBar(title: const Text("NutriMAMA")),
-      body: Center(
-        child: Container(
-          width: 350,
-          height: 450,
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/nutriMAMA.png"),
-              opacity: 0.05,
-              fit: BoxFit.fitWidth,
-            ),
-          ),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  controller: _weghtController,
-                  decoration: const InputDecoration(
-                    labelText: "Вес ребёнка в граммах",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Пожалуйста введите вес ребёнка';
-                    }
-                    final trimmed = value.trim();
-                    final num = int.tryParse(trimmed);
-                    if (num == null || num <= 0) {
-                      return 'Введите корректный вес ребёнка';
-                    }
-                    if (num > 4000) {
-                      return 'Введите корректный вес ребёнка';
-                    }
-                    return null;
-                  },
+      body: Container(
+        child: Stack(
+          children: [
+            //форму позже доавбить
+            Padding(
+              padding: EdgeInsetsGeometry.only(top: 250, left: 35, right: 35, bottom: 250),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _ageController,
-                  decoration: const InputDecoration(
-                    labelText: "Возраст",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Пожалуйста введите возраст в неделях';
-                    }
-                    final trimmed = value.trim();
-                    final num = int.tryParse(trimmed);
-                    if (num == null || num <= 0) {
-                      return 'Введите корректный возраст';
-                    }
-                    if (num > 52) {
-                      return 'Введите возраст до года';
-                    }
-                    return null;
-                  },
-                  //maxLength: 50,
-                ),
-                const SizedBox(height: 4),
-                const Padding(
-                  padding: EdgeInsets.only(left: 12.0),
-                  child: Text(
-                    "в неделях",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                DropdownButtonFormField(
-                  value: _drop,
-                  items: _dropDown.map((String drop) {
-                    return DropdownMenuItem<String>(
-                      value: drop,
-                      child: Text(drop),
-                      );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _drop = newValue!;
-                    });
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Тип вскармливания',
-                    border: OutlineInputBorder()
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Выберите тип вскармливания';
-                    }
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: 24),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: _calculate,
-                    child: const Text("Рассчитать"),
-                    style: ElevatedButton.styleFrom(
-                      textStyle: TextStyle(fontSize: 20),
-                      backgroundColor: const Color.fromARGB(255, 184, 137, 189),
-                      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                      padding: EdgeInsets.all(15),
-                      elevation: 5,
-                      minimumSize: Size(200, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(10),
+                child: Form(
+                  child: Column(
+                    children: [
+                      //форма для ввода имени
+                      Padding(
+                        padding: EdgeInsetsGeometry.only(
+                          top: 10,
+                          left: 15,
+                          right: 15,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Имя ребенка', style: TextStyle(fontSize: 18)),
+                            Padding(padding: EdgeInsetsGeometry.all(5)),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: "Имя",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                                  borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 163, 163, 163),
+                                    width: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+
+                      //форма для ввода веса в граммах
+                      Padding(
+                        padding: EdgeInsetsGeometry.only(
+                          top: 10,
+                          left: 15,
+                          right: 15,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Возраст (в неделях)',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Padding(padding: EdgeInsetsGeometry.all(5)),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: "0-52 недели",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                                  borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 163, 163, 163),
+                                    width: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      //форма для веса ребенка в граммах
+                      Padding(
+                        padding: EdgeInsetsGeometry.only(
+                          top: 10,
+                          left: 15,
+                          right: 15,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Вес ребенка в граммах',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: "Например: 3200",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                                  borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 163, 163, 163),
+                                    width: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+
+            Positioned(
+              top: 60,
+              left: 30,
+              child: Image.asset(
+                "assets/images/nutri_Logo.png",
+                width: 150,
+                height: 150,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
         ),
       ),
     );
