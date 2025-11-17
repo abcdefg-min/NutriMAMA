635c6a0dd7c68d8477e1017450ee484a4b9bf13a
abcdefg_minSUng <vikavolk531@mail.ru>
2025-11-17 14:43:26 +0300

    пока убрала firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_nutrimama/nutri_mama_screen.dart';
-import 'firebase_options.dart';
+//import 'firebase_options.dart';
 import 'auth_screen.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    //options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp(
